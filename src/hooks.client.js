54d92085d73c766ffb915182c3cd25d4828ccb53
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2025-09-18 09:19:03 +0300

    ffg

--- src/hooks.client.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('✅ Service worker registered');
-  }).catch((err) => {
-    console.error('SW registration failed', err);
-  });
-}
